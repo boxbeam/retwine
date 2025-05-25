ed209c15e0fcc6a6c0bda849b1e1ded67d3d7036
boxbeam <instantiable@gmail.com>
2025-05-25 18:09:28 -0400

    Initialization

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+// sample syntax
+//
+// retwine!(if let "number": num=%d* = str {
+
+// } else {
+
+// });
+
+// retwine!(match str {
+//     "number:" num=%d* => {
+
+//     }
+// });
+
+// retwine!(let "number": num=%d* = str else {
+//     panic!()
+// });
